62ceb09550c7cd5250f0fe89073706f1e166bf96
Smeza314 <sebastianmeza314@gmail.com>
2021-04-24 19:38:28 -0700

    installed all npms

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const compression = require("compression");
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(compression());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+});
+
+// routes
+app.use(require("./routes/api.js"));
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`App running on port 3000!`);
+});
+© 2021 GitHub, Inc.
